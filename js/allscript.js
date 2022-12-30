1dcff78fa619262b5db907a2d46776f16fc78a93
damitha jayatharaka <hellodamitha@gmail.com>
2022-12-31 02:23:05 +0530

    product page

--- js/allscript.js
+++ js/allscript.js
@@ -52,3 +52,26 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 })
+
+
+
+
+
+
+// --------------------------------------------product main page js----------------------------------
+
+let tabs = document.querySelectorAll('.tabs__toggle'),
+    contents = document.querySelectorAll('.tabs__content');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        contents.forEach((content) => {
+            content.classList.remove('is-active');
+        });
+        tabs.forEach((tab) => {
+            tab.classList.remove('is-active');
+        });
+        contents[index].classList.add('is-active');
+        tabs[index].classList.add('is-active');
+    });
+});
